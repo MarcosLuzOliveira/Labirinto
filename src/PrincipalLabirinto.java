34172c3d316f272b6d476aa88f8cf897e7af1a95
Marcos Oliveira <marcosluz312@hotmail.com>
2021-04-28 00:08:36 -0300

    close

--- src/PrincipalLabirinto.java
+++ src/PrincipalLabirinto.java
@@ -12,16 +12,17 @@ public class PrincipalLabirinto {
 				Scanner scanner = new Scanner(System.in);
 				String nome;
 				Labirinto2 l = new Labirinto2();
-				System.out.println("Dígite o nome do arquivo do labirinto (temos 2 exemplos M2F.txt e m2LAB)");
+				System.out.println("Dï¿½gite o nome do arquivo do labirinto (temos 2 exemplos M2F.txt e m2LAB)");
 				nome = scanner.nextLine();
 				l.LC(nome);
 				resultado = l.labirinto(l.carregaLabirinto(nome));
-				if(resultado) {
+				if(resultado) 
 					out.println("Existe um caminho para o labirinto");
-				}else
-					out.println("Não existe um caminho para o labirinto");
+				else
+					out.println("Nï¿½o existe um caminho para o labirinto");
 				out.close();
-			}catch(IOException e) {
+				scanner.close();
+				}catch(IOException e) {
 				System.out.println("Erro ao escrever arquivo");
 			}
 			
